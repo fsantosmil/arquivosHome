452e25073911adddd66d23dc15d5f761f8b6a138
FabianoSouza <fsantosmil@gmail.com>
2018-03-12 20:09:43 -0300

    Dash01

---
+++ DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+use Request;
+Use App\Dashboard;
+use Yajra\Datatables\Datatables;
+
+
+class DashboardController extends Controller
+{
+    
+    public function index(){
+        return view('features');
+    }
+
+    
+    public function addGamesPsn() {
+
+    }
+
+    public function listarGames() {
+
+    }
+
+
+}
